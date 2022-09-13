fc17e39a76d22ca97353e0cfee2f120ccc844848
Hansol Cho <zwwz11@naver.com>
2022-09-13 20:22:08 +0900

    초기 커밋.

---
+++ DesignPattenExample/SingletonExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattenExample
+{
+    class SingletonExample
+    {
+        private static SingletonExample singleton;
+        public static SingletonExample Instance() 
+        { 
+            if(singleton == null)
+            {
+                singleton = new SingletonExample();
+            }
+
+            return singleton;
+        }
+    }
+}
